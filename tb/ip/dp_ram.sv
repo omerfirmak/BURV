73f16b3f190a6e13a6e150ef0b743d0cdfd8603b
Omer Faruk IRMAK <omerfirmak@gmail.com>
2018-10-24 19:47:11 +0300

    Reverted changes

--- tb/ip/dp_ram.sv
+++ tb/ip/dp_ram.sv
@@ -24,27 +24,27 @@ module dp_ram
 
     localparam words = 1024;
 
-    logic [31:0] mem[words];
+    logic [3:0][7:0] mem[words];
 
     always @(posedge clk) begin
         a_ready_o <= 0;
         b_ready_o <= 0;
         
         if (a_valid_i && !a_ready_o) begin
-            if (a_we_i[0]) mem[a_addr_i >> 2][7 : 0] <= a_wdata_i[7 : 0];
-            if (a_we_i[1]) mem[a_addr_i >> 2][15 : 8] <= a_wdata_i[15 : 8];
-            if (a_we_i[2]) mem[a_addr_i >> 2][23 : 16] <= a_wdata_i[23 : 16];
-            if (a_we_i[3]) mem[a_addr_i >> 2][31 : 24] <= a_wdata_i[31 : 24];
+            if (a_we_i[0]) mem[a_addr_i >> 2][0] <= a_wdata_i[7:0];
+            if (a_we_i[1]) mem[a_addr_i >> 2][1] <= a_wdata_i[15:8];
+            if (a_we_i[2]) mem[a_addr_i >> 2][2] <= a_wdata_i[23:16];
+            if (a_we_i[3]) mem[a_addr_i >> 2][3] <= a_wdata_i[31:24];
 
             a_rdata_o <= mem[a_addr_i >> 2];
             a_ready_o <= 1;
         end
 
         if (b_valid_i && !b_ready_o) begin
-            if (b_we_i[0]) mem[b_addr_i >> 2][7 : 0] <= b_wdata_i[7 : 0];
-            if (b_we_i[1]) mem[b_addr_i >> 2][15 : 8] <= b_wdata_i[15 : 8];
-            if (b_we_i[2]) mem[b_addr_i >> 2][23 : 16] <= b_wdata_i[23 : 16];
-            if (b_we_i[3]) mem[b_addr_i >> 2][31 : 24] <= b_wdata_i[31 : 24];
+            if (b_we_i[0]) mem[b_addr_i >> 2][0] <= b_wdata_i[7:0];
+            if (b_we_i[1]) mem[b_addr_i >> 2][1] <= b_wdata_i[15:8];
+            if (b_we_i[2]) mem[b_addr_i >> 2][2] <= b_wdata_i[23:16];
+            if (b_we_i[3]) mem[b_addr_i >> 2][3] <= b_wdata_i[31:24];
 
             b_rdata_o <= mem[b_addr_i >> 2];
             b_ready_o <= 1;
